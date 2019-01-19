bbf9340ad376bfa32744a1f9413816cc92cca4e3
Élisson Michael <elissonmichael@yahoo.com.br>
2019-01-18 21:05:14 -0300

    Add Schema and Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Character.create(name: 'Elisson', strength: 10, constitution: 3, dexterity: 6,
+                 intelligence: 8, wisdom: 10, charisma: 3)
+Character.create(name: 'Satya', strength: 3, constitution: 3, dexterity: 5,
+                 intelligence: 15, wisdom: 10, charisma: 8)
