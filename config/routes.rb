7a2c510a3c24cab5bc2c5d49388697e3271568f0
Élisson Michael <elissonmichael@gmail.com>
2019-11-25 11:18:37 -0300

    Add Root to Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   namespace :v1 do
     defaults format: :json do
       resources :characters do
-        resources :skills, except: [:index, :show] 
+        resources :skills, except: [:index, :show]
       end
+      root 'characters#index'
     end
+  end
+  defaults format: :json do
+    root 'v1/characters#index'
   end
 end
